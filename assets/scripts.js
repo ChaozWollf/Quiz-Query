ba23f807f896136539e3fc14f1fa9c2dead6c5ca
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-04-28 17:18:55 -0500

    worked on storage of initials and high score

--- assets/scripts.js
+++ assets/scripts.js
@@ -18,6 +18,8 @@
 //if else statment for wrong answers to take off time
 //end game variable when the timer reaches 0
 //object pushed to local storage with initials tied to highscore
+
+let hss = [];
 const startBtn = document.querySelector('.startEle')
 const timer = document.body.querySelector(".timer");
 const hs = document.querySelector(".HS");
@@ -28,27 +30,27 @@ let mode = preGame;
 
 const questions = [
   {
-    questions: 'String statments are enclosed within __________ marks',
+    question: 'String statments are enclosed within __________ marks',
     choices: ['quotation', 'paragraph', 'track', 'bite', 'none of the above'],
     answer: 'quotation'
   },
   {
-    questions: 'The previous question was a little _________',
+    question: 'The previous question was a little _________',
     choices: ['dark', 'twisted', 'inaproperiate', 'made out of frustration', 'all of the above'],
     answer: 'all of the above'
   },
   {
-    questions: 'An append statment adds to an element on the _________',
+    question: 'An append statment adds to an element on the _________',
     choices: ['top of the listed element(ex.by reading from top to bottom "appended element then listed element"', 'bottom of the listed element', 'restraining order list'],
     answer: 'bottom of the listed element'
   },
   {
-    questions: 'An alert is ment to______?',
+    question: 'An alert is ment to______?',
     choices: ['inform the enduser of possible problems', 'ask the enduser for input', 'annoy the enduser', 'add a element to the page', 'cause a massive surge in power so that the enduser can no longer operate their computer'],
     answer: 'inform the enduser of possible problems'
   },
   {
-    questions: 'A console.log() is will______',
+    question: 'A console.log() is will______',
     choices: ['log the thing in the parenths in the console', 'make an array', 'make a function', 'log your personal gaming console at home', 'take over the world'],
     answer: 'log the thing in the parenths in the console'
   }
@@ -57,7 +59,7 @@ const questions = [
 function startQuiz() {
   questions.forEach(function (questionObj) {
     const questionEl = document.createElement("div");
-    questionEl.textContent = questionObj.questions;
+    questionEl.textContent = questionObj.question;
     questionsEl.appendChild(questionEl);
   });
 }
@@ -67,22 +69,37 @@ function startTimer() {
   const timerInterval = setInterval(function () {
     timer.textContent = "Timer: " + timeLeft;
     timeLeft--;
-    if (timeLeft == 0) {
+    if (timeLeft == -1) {
       clearInterval(timerInterval);
-      endGame();
+      endGame(timeLeft);
     }
   }, 1000);
 }
 function hideStart() {
-  startButton
+  startBtn.style.display= "none";
+}
 
   
+
+function renderEnder () { 
+  const initials = JSON.parse(localStorage.getItem("hss"));
+  return initials;
 }
 
 
-function endGame() {
-  const hss = prompt("initials please");
+function endGame(timeLeft) {
+  const initials = prompt("initials please");
+  const score = { initials: initials, timeLeft: timeLeft };
+  hss.push(score); 
+  localStorage.setItem ("hss",JSON.stringify (hss));
+renderEnder();
 }
- startBtn.addEventListener('click', startQuiz);
+ 
+
+
+
+
+
+startBtn.addEventListener('click', startQuiz);
   startBtn.addEventListener('click', startTimer);
 startBtn.addEventListener('click', hideStart);
