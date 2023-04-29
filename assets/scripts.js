6ab1bb0091cc3304940dade92ce37a7efbf110c4
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-04-29 08:10:30 -0500

    polished my code a little

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,4 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
 
-
-//function for the timer
-//variable for the questions
-//start button linked to timer in JS and question const
-//question textContent and a button for possible the answers
-//if else statment for wrong answers to take off time
-//end game variable when the timer reaches 0
-//object pushed to local storage with initials tied to highscore
 let timerInterval;
 let timeLeft = 60;
 let hss = [];
